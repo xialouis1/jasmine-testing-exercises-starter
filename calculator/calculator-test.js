586b65e74e454cd26a39fe38ea7dd61ad1ab12f7
Louis Xia <23242389+xialouis1@users.noreply.github.com>
2024-03-12 22:08:18 -0400

    Loan Calculator
    Built the functionality for calculator which calculates the monthly payment given a principle amount, yearly rate, and number of years.

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,28 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  let values = {
+    amount: 100,
+    rate: 12,
+    years: 1
+  }
+
+  let monthlyPayment = calculateMonthlyPayment(values);
+
+  expect(monthlyPayment).toEqual('100.02');
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  let values = {
+    amount: 100,
+    rate: 12,
+    years: 1
+  }
+
+  let monthlyPayment = calculateMonthlyPayment(values);
+
+  let decimal = monthlyPayment.split('.')[1];
+
+  expect(decimal).toEqual('02');
 });
 
-/// etc
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,20 +21,38 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  let loanAmount = document.querySelector('#loan-amount');
+  loanAmount.value = '550000';
+
+  let loanYears = document.querySelector('#loan-years');
+  loanYears.value = '30';
+
+  let loanRate = document.querySelector('#loan-rate');
+  loanRate.value = '7.25';
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  let values = getCurrentUIValues();
+  updateMonthly(calculateMonthlyPayment(values));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let P = Number(values.amount);
+  let i = Number(values.rate / 12);
+  let n = Number(values.years * 12);
+
+  let monthlyPayment = (P * i) / (1 - Math.pow((1 + i), -n));
+
+  return monthlyPayment.toFixed(2);
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  document.querySelector('#monthly-payment').innerHTML = `$${monthly}`;
 }
